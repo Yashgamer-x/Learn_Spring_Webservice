12386d2112cacd451ca7ab5c021ebe47b109bfa9
Yashgamer-x <yashdesai121212@gmail.com>
2025-09-17 18:17:34 -0400

    Initial Commit

---
+++ src/main/java/org/yashgamerx/learn_restful_webservice/LearnRestfulWebserviceApplication.java
@@ -0,0 +1,13 @@
+package org.yashgamerx.learn_restful_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRestfulWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnRestfulWebserviceApplication.class, args);
+    }
+
+}
