2dc29699dbe21021bd0a8abdf51103d4bec1ca8b
Yashgamer-x <yashdesai121212@gmail.com>
2025-09-17 18:19:08 -0400

    No more need of public in Java 25

--- src/main/java/org/yashgamerx/learn_restful_webservice/LearnRestfulWebserviceApplication.java
+++ src/main/java/org/yashgamerx/learn_restful_webservice/LearnRestfulWebserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearnRestfulWebserviceApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(LearnRestfulWebserviceApplication.class, args);
     }
 
